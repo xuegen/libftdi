4d013421b98c56159cbdbb66aff51e0e60db9a5e
Xuegen Zhu <xuegen.zhu@keysight.com>
2023-07-14 18:33:09 -0700

    add setup.sh for package installation

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# git://developer.intra2net.com/libftdi
+sudo apt-get install -y cmake
+sudo apt-get install -y build-essential
+sudo apt-get install -y git-core
+sudo apt-get install -y cmake
+sudo apt-get install -y doxygen
+sudo apt-get install -y libusb-1.0-0-dev
+
+# for ftdi-eeprom
+sudo apt-get install -y libconfuse-dev
+
+# for python bindings
+sudo apt-get install -y swig
+sudo apt-get install -y python-dev
+
+# for C++ binding and unit test
+sudo apt-get install -y libboost-all-dev
